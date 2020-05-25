74b4cb06190533f550e1450f2b4a17765b2acdab
eito0201 <eito.k.8888@gmail.com>
2020-05-25 17:03:57 +0900

    implement action and action creator

---
+++ src/actions/index.js
@@ -0,0 +1,10 @@
+export const INCLREMENT = 'INCREMENT'
+export const DECREMENT = 'DECREMENT'
+
+export const increment = () => ({
+  type: INCLREMENT
+})
+
+export const decrement = () => ({
+  type: DECREMENT
+})
